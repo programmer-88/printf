b6265b5ca2207535be064e42eaa872348c676e6d
programmer-88 <ianmbayyz@gmail.com>
2023-08-22 21:17:31 +0300

    fixed _printf

--- _printf.c
+++ _printf.c
@@ -8,39 +8,43 @@
 
 int _printf(const char *format, ...)
 {
+	unsigned int i, s_c = 0, c = 0;
 	va_list arg;
-	int c = 0;
 
 	va_start(arg, format);
+	if (format == NULL)
+		return (-1);
 
-	while (*format != '\0')
+	for (i = 0; format[i] != '\0'; i++)
 	{
-	if (*format == '%')
-	{
-		format++;
-		switch (*format)
-	{
-		case 'c':
-			c += printf("%c", va_arg(arg, int));
-			break;
-		case 's':
-			c += printf("%s", va_arg(arg, char*));
-			break;
-		case '%':
-			c += printf("%%");
-			break;
-		default:
-			break;
-	}
-	}
-	else
-	{
-		printf("%c", *format);
+		if (format[i] != '%')
+		{
+			putchar(format[i]);
+		}
+		else if (format[i + 1] == 'c')
+		{
+			putchar(va_arg(arg, int));
+			i++;
+		}
+		else if (format[i + 1] == 's')
+		{
+			char *str = va_arg(arg, char *);
+
+			while (*str)
+			{
+				putchar(*str);
+				str++;
+				s_c++;
+			}
+			i++;
+			c += (s_c - 1);
+		}
+		else if (format[i + 1] == '%')
+		{
+			putchar('%');
+		}
 		c++;
 	}
-	format++;
-	}
-
 	va_end(arg);
 	return (c);
 }
